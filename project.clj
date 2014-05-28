cd81f8eed70d82be21817f85388917b6d0b367ad
Jason Gilman <jason@element84.com>
2014-05-27 20:59:38 -0400

    What we came up with.

--- project.clj
+++ project.clj
@@ -3,9 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  
-  :profiles 
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [criterium "0.4.3"]]
+
+  :jvm-opts ^:replace ["-server"
+                       ;; Uncomment this to enable assertions. Turn off during performance tests.
+                       "-ea"
+                       "-Dcom.sun.management.jmxremote"
+                       "-Dcom.sun.management.jmxremote.ssl=false"
+                       "-Dcom.sun.management.jmxremote.authenticate=false"
+                       "-Dcom.sun.management.jmxremote.port=1098"]
+
+
+  :profiles
   {:dev {:source-paths ["dev" "src"]
          :dependencies [[org.clojure/tools.namespace "0.2.4"]
                         [org.clojars.gjahad/debug-repl "0.3.3"]]}})
--- src/mr_challenjer/guessers.clj
+++ src/mr_challenjer/guessers.clj
@@ -1,13 +1,16 @@
 (ns mr-challenjer.guessers
-  "Defines various guesser functions that implement different strategies. A guesser function will be 
+  "Defines various guesser functions that implement different strategies. A guesser function will be
   passed a the current guess state containing the following keys:
-  
+
   * word - A vector of the current word state. It contains characters of correctly guessed letters
   or _ (underscore) as placeholders.
   * guesses - a sequence of past guessed characters.
   * tries-left - the number of incorrect guesses left."
   (:require [mr-challenjer.core :as mr]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [criterium.core :as criterium]))
 
 
 (def always-a
@@ -30,25 +33,97 @@
       (a-z guess-state)
       (first (filter (complement past-guess-set) vowels)))))
 
-(comment 
+(defn simple-english-freq
+  [guess-state]
+  (let [alphabet [\e \t \a \o \i \n \s \r \h \d \l \u
+                  \c \m \f \y \w \g \p \b \v \k \x \q \j \z]
+        past-guess-set (set (:guesses guess-state))]
+    (first (filter (complement past-guess-set) alphabet))))
+
+(def all-the-words
+  (with-open [rdr (io/reader mr/dictionary-file)]
+    (vec (line-seq rdr))))
+
+(def all-the-words-vecs
+  (mapv vec all-the-words))
+
+(defn word-matches?
+  [pattern word]
+  (re-matches pattern word))
+
+(defn choose-letter
+  [pattern possibles guesses]
+  (let [guesses (set guesses)
+        pos-letter-freq (for [n (range (count pattern))]
+                          (frequencies (filterv (complement guesses)
+                                               (mapv #(nth % n) possibles))))
+        underscore-pos (ffirst (filterv #(= \_ (second %))
+                                               (map-indexed vector pattern)))
+        letter-freq (nth pos-letter-freq underscore-pos)]
+    (->> letter-freq
+         set/map-invert
+         (sort-by first)
+         reverse
+         first
+         second)))
+
+(defn possible-matches
+  "Given a set of possible words and the word vector
+  returns the subset of possible words that match the word vector"
+  [pos-word-vec word-vec]
+  (let [pattern (re-pattern
+                  (str "^"
+                       (str/replace (apply str word-vec) "_" ".")
+                       "$"))]
+    (filterv (partial word-matches? pattern) pos-word-vec)))
+
+(defn dictionary-attack
+  [{:keys [guesses word extra-state]}]
+  (when-not extra-state
+    (println "Extra state not set"))
+  (let [possibles (possible-matches (or extra-state
+                                        all-the-words)
+                                    word)]
+    {:guess (choose-letter word possibles guesses)
+     :extra-state possibles}))
+
+
+
+(comment
   (def test-guess-state
     {:word [\_ \_ \_ \_]
      :guesses [\a \b]
      :tries-left 6})
 
-  ;; Test calling guesser function 
+  ;; Test calling guesser function
   (always-a test-guess-state)
   (a-z test-guess-state)
-  
+
+  (dictionary-attack test-guess-state)
+
   ;; Test a guesser with a single word
   (mr/test-guesser always-a)
   (mr/test-guesser a-z)
   (mr/test-guesser vowels-then-a-z)
-  
+  (time (assoc-in (mr/test-guesser dictionary-attack)
+                  [:guess-state :extra-state]
+                  nil))
+
+  (time
+    (dotimes [n 10]
+      (mr/test-guesser dictionary-attack "abracadabra")))
+
+  (mr/measure-guesser (fn [_]
+                        (println "called")
+                        \e))
+
+
   ;; Get the percentages success rate of a guesser
   (mr/measure-guesser always-a)
   (mr/measure-guesser a-z 1000)
   (mr/measure-guesser vowels-then-a-z 1000)
-  
-  
+  (mr/measure-guesser dictionary-attack 100)
+
+  (mr/measure-guesser simple-english-freq)
+
   )
